6785a3964710cdc9ca18aa47412d6807bd10da92
Johnnyy91 <claudiosardilli@gmail.com>
2023-01-26 16:24:25 +0100

    food

--- src/org/lessons/java/food/Food.java
+++ src/org/lessons/java/food/Food.java
@@ -1,5 +1,11 @@
 package org.lessons.java.food;
 
 public class Food {
-
+	public static void main(String[] args) {
+		String[] food = {"carbonara","lasagna","pizza","tiramisù","Tagliata","pesce"};
+		
+		System.out.println(food[0]);
+		System.out.println(food[food.length /2 - 1]);
+		System.out.println(food[food.length - 1]);
+	}
 }
